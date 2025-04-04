4f1f57990c7ba55d1666bb389f73d96fbe05a0ba
jessicasousav <jessiesousavi@gmail.com>
2025-04-03 21:27:04 -0300

    update cores T2

--- t2_transformDados.py
+++ t2_transformDados.py
@@ -7,8 +7,14 @@ from time import sleep
 inputA1 = "Anexo1.pdf"
 outputA1 = "Anexo1.csv"
 
+cores = {
+    "verde": "\033[32m",
+    "laranja": "\033[33m",
+    "lilas": "\033[94m"
+}
+
 # Extraindo dados da tabela:
-print("\nExtraindo dados da sua tabela...(1/4)")
+print(f"\n{cores['lilas']}Extraindo dados da sua tabela... (1/4)\033[m")
 
 originalA1 = tabula.read_pdf(input_path=inputA1, pages="all", encoding='utf-8', lattice=True, stream=True)
 
@@ -17,7 +23,7 @@ updatedA1 = pd.concat(originalA1, ignore_index=True)
 
 # Substituindo nomes COLUNAS:
 sleep(1)
-print("\nSubstituindo abreviações necessárias...(2/4)")
+print(f"\n{cores['lilas']}Substituindo abreviações necessárias... (2/4)\033[m")
 
 updatedA1.rename(
     columns = {
@@ -43,17 +49,18 @@ print(df)
 
 # salvando em csv:
 sleep(1)
-print("\nConvertendo seu arquivo para csv...(3/4)")
+print(f"\n{cores['lilas']}Convertendo seu arquivo para csv... (3/4)\033[m")
 updatedA1.drop(columns=updatedA1.columns[0], axis=1, inplace=True)
 updatedA1.to_csv(outputA1, index=False, encoding='utf-8')
 
 
 # compactando para zip:
 sleep(1)
-print("\nCompactando seu arquivo...(4/4)")
+print(f"\n{cores['lilas']}Compactando seu arquivo... (4/4)\033[m")
 
 nome = "Jessica_Vieira"
 with ZipFile(f"Teste_{nome}.zip", "w") as zip:
     zip.write(outputA1)
 
-print("Arquivo csv compactado com sucesso!\n")
+print(f"{cores['verde']}Arquivo csv compactado com sucesso!\033[m")
+print(f"\n{cores['laranja']}Finalizando a tarefa!!\033[m\n")
